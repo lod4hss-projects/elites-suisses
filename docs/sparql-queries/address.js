0e2cf54ddc2720d541415dc199d5ff933bdc8572
Francesco Beretta <francesco.beretta@cnrs.fr>
2026-08-20 22:36:57 +0200

    infos sur l'instance sujet incoming

--- docs/sparql-queries/address.js
+++ docs/sparql-queries/address.js
@@ -1,1 +1,2 @@
-let address =  'https://swiss-elites.lod4hss.cloud/wisski/endpoint/default_wisski_distillery_adapter' ;
+// verify always the correct URL
+let address='https://swiss-elites.lod4hss.cloud/wisski/endpoint/default_wisski_distillery_adapter' ;
